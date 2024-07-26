dc897cb92d743a66825f1047d709da6d0e1eb833
Maccheww <matthew_wassmer@dlsu.edu.ph>
2024-07-26 11:19:25 +0800

    Added detailed patient screen with prescriptions (it looks ugly, but it works)

--- lib/screens/doctor/patient_details_screen.dart
+++ lib/screens/doctor/patient_details_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:codingbryant/blocs/user_bloc/auth_bloc.dart'; // Adjust import as per your project structure
 
 class PatientDetailsScreen extends StatelessWidget {
@@ -15,7 +15,7 @@ class PatientDetailsScreen extends StatelessWidget {
       body: BlocBuilder<AuthBloc, AuthState>(
         builder: (context, state) {
           if (state is AuthAuthenticated) {
-            // Check if patient exists in Firestore
+            // Fetch patient details
             return FutureBuilder<QuerySnapshot>(
               future: FirebaseFirestore.instance
                   .collection('patients')
@@ -37,99 +37,166 @@ class PatientDetailsScreen extends StatelessWidget {
                 }
 
                 // Patient found, assume there's only one matching document
-                var data =
+                var patientData =
                     snapshot.data!.docs.first.data() as Map<String, dynamic>;
                 String phoneNumber =
-                    data['phone_number'] ?? 'Phone number not available';
-                String email = data['email'] ?? 'Email not available';
+                    patientData['phone_number'] ?? 'Phone number not available';
+                String email = patientData['email'] ?? 'Email not available';
 
-                // Build UI with retrieved data
-                return NestedScrollView(
-                  headerSliverBuilder:
-                      (BuildContext context, bool innerBoxIsScrolled) {
-                    return <Widget>[
-                      SliverAppBar(
-                        pinned: true,
-                        floating: true,
-                        snap: false,
-                        backgroundColor: Colors.blue,
-                        expandedHeight:
-                            MediaQuery.of(context).size.height * 2 / 9,
-                        flexibleSpace: FlexibleSpaceBar(
-                          title: Text(
-                            patientName,
-                            style: TextStyle(
-                              fontFamily: 'RobotoMono',
-                              fontSize: 32,
-                              fontWeight: FontWeight.w900,
-                              color: Colors.white,
-                            ),
-                          ),
-                          centerTitle: true,
-                        ),
-                      ),
-                    ];
-                  },
-                  body: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
+                // Fetch prescriptions for the patient
+                return FutureBuilder<QuerySnapshot>(
+                  future: FirebaseFirestore.instance
+                      .collection('prescriptions')
+                      .where('patientId', isEqualTo: patientName)
+                      .get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> prescriptionsSnapshot) {
+                    if (prescriptionsSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    if (prescriptionsSnapshot.hasError) {
+                      return Center(
                           child: Text(
-                            'Patient Details Section',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
+                              'Error: ${prescriptionsSnapshot.error.toString()}'));
+                    }
+
+                    // Prepare prescriptions data
+                    List<QueryDocumentSnapshot> prescriptions =
+                        prescriptionsSnapshot.data!.docs;
+
+                    // Build UI with retrieved data
+                    return NestedScrollView(
+                      headerSliverBuilder:
+                          (BuildContext context, bool innerBoxIsScrolled) {
+                        return <Widget>[
+                          SliverAppBar(
+                            pinned: true,
+                            floating: true,
+                            snap: false,
+                            backgroundColor: Colors.blue,
+                            expandedHeight:
+                                MediaQuery.of(context).size.height * 2 / 9,
+                            flexibleSpace: FlexibleSpaceBar(
+                              title: Text(
+                                patientName,
+                                style: TextStyle(
+                                  fontFamily: 'RobotoMono',
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w900,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              centerTitle: true,
                             ),
                           ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 8),
-                          padding: const EdgeInsets.all(16),
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.grey,
-                                spreadRadius: 2,
-                                blurRadius: 7,
-                                offset: Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Patient Name: $patientName',
+                        ];
+                      },
+                      body: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Patient Details Section',
                                 style: TextStyle(
-                                  color: Colors.black,
+                                  fontSize: 24,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              SizedBox(height: 10),
-                              Text(
-                                'Phone Number: $phoneNumber',
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 8),
+                              padding: const EdgeInsets.all(16),
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.grey,
+                                    spreadRadius: 2,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Patient Name: $patientName',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Phone Number: $phoneNumber',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Email: $email',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            // Display prescriptions
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Text(
+                                'Prescriptions',
                                 style: TextStyle(
-                                  color: Colors.black,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              SizedBox(height: 10),
-                              Text(
-                                'Email: $email',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                            const SizedBox(height: 10),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: prescriptions.length,
+                              itemBuilder: (context, index) {
+                                var prescription = prescriptions[index].data()
+                                    as Map<String, dynamic>;
+                                double prescriptionDosage =
+                                    prescription['dosage'] ??
+                                        'Dosage not available';
+                                String prescriptionDosageString =
+                                    prescriptionDosage.toStringAsFixed(
+                                        2); // Format dosage to 2 decimal places
+                                // Customize how you display prescription details
+                                return ListTile(
+                                    title: Text(
+                                        'Medicine Name: ${prescription['medication']}'),
+                                    subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                            'Dosage: $prescriptionDosageString mg'),
+                                        Text(
+                                            'Instructions: ${prescription['instructions']}'),
+                                      ],
+                                    ));
+                              },
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
+                    );
+                  },
                 );
               },
             );
