5575294479924440bd42abeba0dd9926f9b46422
Maccheww <matthew_wassmer@dlsu.edu.ph>
2024-07-29 14:21:51 +0800

    Added template pages for viewing information

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('main.ejs', {username: 'Guest'});
+    res.render('main.ejs');
 });
 
-app.post('/greet', (req, res) => {
-    const { username } = req.body;
-    res.render('main.ejs', { username });
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'admin' && password === 'admin') {
+        // Redirect if successful login
+        res.redirect('/home');
+    } else {
+        res.send('Login Failed');
+    }
+});
+
+app.get('/home', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get('/patients', (req, res) => {
+    res.render('patients.ejs');
 });
 
 app.listen(port, () => {
